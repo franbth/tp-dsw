5a8f890059489bfdfd46d4673d4dd4e66bae5ed3
Francisco Buthet <franciscobuthet@gmail.com>
2024-05-09 16:20:09 -0300

    Primer config

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+app.use('/', (req, res) => {
+    res.json({ message: '<h1>Hello</h1>' });
+});
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:300/");
+});
+//# sourceMappingURL=app.js.map
